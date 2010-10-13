f05c85ad82d3bae374e2f3d033c5ff918abe60fd
Brian Johnson <brijohn@gmail.com>
2010-10-13 13:23:51 -0400

    Added interactive mode to command line tool
    Signed-off-by: Brian Johnson <brijohn@gmail.com>

--- src/main.c
+++ src/main.c
@@ -33,13 +33,25 @@
 #define CMD_SEND	0x4
 #define CMD_DELETE	0x8
 #define CMD_CAPACITY	0x10
+#define CMD_DISCONNECT	0x20
+#define CMD_CONNECT	0x40
+#define CMD_SCAN	0x80
+
 #define CMD_MASK	0xff
+#define REQ_CON_MASK	(CMD_LIST      | \
+			CMD_MODEL      | \
+			CMD_SEND       | \
+			CMD_DELETE     | \
+			CMD_CAPACITY   | \
+			CMD_DISCONNECT)
 
 #define DEV_SD		0x0100
 #define DEV_INTERNAL	0x0200
+#define CMD_INTERACTIVE 0x8000
+
 uint8_t  debug_level;
 uint16_t command;
-uint8_t  do_scan;
+uint16_t vid, pid;
 char    *usb_device;
 char    *filename;
 
@@ -60,7 +72,9 @@ struct poptOption options[] = {
         "get device model" },
         { "capacity", 0, POPT_BIT_SET, &command, CMD_CAPACITY,
         "get device capacity" },
-        { "scan", 0, 0, &do_scan, 1,
+        { "interactive", 0, POPT_BIT_SET, &command, CMD_INTERACTIVE,
+        "interactive mode" },
+        { "scan", 0, POPT_BIT_SET, &command, CMD_SCAN,
         "scan system for device ids" },
         { "file", 0, POPT_ARG_STRING, &filename, 0,
         "file name to transfer/delete" },
@@ -115,9 +129,6 @@ int list_files(exword_t *d)
 	int rsp, i;
 	directory_entry_t *entries;
 	uint16_t len;
-	rsp = exword_connect(d);
-	if (rsp != 0x20)
-		goto fail;
 	if (command & DEV_SD)
 		rsp = exword_setpath(d, SD_CARD);
 	else
@@ -134,7 +145,6 @@ int list_files(exword_t *d)
 			printf("%s\n", entries[i].name);
 	}
 	free(entries);
-	rsp = exword_disconnect(d);
 fail:
 	return rsp;
 }
@@ -143,9 +153,6 @@ int display_capacity(exword_t *d)
 {
 	int rsp;
 	exword_capacity_t cap;
-	rsp = exword_connect(d);
-	if (rsp != 0x20)
-		goto fail;
 	if (command & DEV_SD)
 		rsp = exword_setpath(d, SD_CARD);
 	else
@@ -153,9 +160,6 @@ int display_capacity(exword_t *d)
 	if (rsp != 0x20)
 		goto fail;
 	rsp = exword_get_capacity(d, &cap);
-	if (rsp != 0x20)
-		goto fail;
-	rsp = exword_disconnect(d);
 	if (rsp != 0x20)
 		goto fail;
 	if (command & DEV_SD)
@@ -173,9 +177,6 @@ int send_file(exword_t *d, char *filename)
 	rsp = read_file(filename, &buffer, &len);
 	if (rsp != 0x20)
 		goto fail;
-	rsp = exword_connect(d);
-	if (rsp != 0x20)
-		goto fail;
 	if (command & DEV_SD)
 		rsp = exword_setpath(d, SD_CARD);
 	else
@@ -183,9 +184,6 @@ int send_file(exword_t *d, char *filename)
 	if (rsp != 0x20)
 		goto fail;
 	rsp = exword_send_file(d, basename(filename), buffer, len);
-	if (rsp != 0x20)
-		goto fail;
-	rsp = exword_disconnect(d);
 fail:
 	free(buffer);
 	return rsp;
@@ -195,10 +193,7 @@ int delete_file(exword_t *d, char *filename)
 {
 	int rsp;
 	if (filename == NULL)
-		return 0x50;
-	rsp = exword_connect(d);
-	if (rsp != 0x20)
-		goto fail;
+		return 0x40;
 	if (command & DEV_SD)
 		rsp = exword_setpath(d, SD_CARD);
 	else
@@ -206,9 +201,6 @@ int delete_file(exword_t *d, char *filename)
 	if (rsp != 0x20)
 		goto fail;
 	rsp = exword_remove_file(d, basename(filename));
-	if (rsp != 0x20)
-		goto fail;
-	rsp = exword_disconnect(d);
 fail:
 	return rsp;
 }
@@ -218,18 +210,178 @@ int display_model(exword_t *d)
 	int rsp;
 	uint16_t len = 30;
 	char model[30];
-	rsp = exword_connect(d);
-	if (rsp != 0x20)
-		goto fail;
 	rsp = exword_get_model(d, model, &len);
-	if (rsp != 0x20)
-		goto fail;
-	rsp = exword_disconnect(d);
 	if (rsp != 0x20)
 		goto fail;
 	printf("Model: %s\n", model);
 fail:
 	return rsp;
+}
+
+int connect(exword_t *d)
+{
+	int rsp;
+	rsp = exword_connect(d);
+	return rsp;
+}
+
+int disconnect(exword_t *d)
+{
+	int rsp;
+	rsp = exword_disconnect(d);
+	return rsp;
+}
+
+int parse_commandline(char *cmdl)
+{
+	int ret = 0;
+	char *cmd = NULL;
+	command &= ~CMD_MASK;
+	sscanf(cmdl, "%ms", &cmd);
+	if (strcmp(cmd, "scan") == 0) {
+		command |= CMD_SCAN;
+	} else if (strcmp(cmd, "help") == 0) {
+		printf("Commands:\n");
+		printf("scan               - List Devices\n");
+		printf("connect <vid:pid>  - connect to dictionary\n");
+		printf("disconnect         - disconnect to dictionary\n");
+		printf("model              - print model\n");
+		printf("storage <sd|mem>   - switch storage medium\n");
+		printf("list               - list files\n");
+		printf("capacity           - display medium capacity\n");
+		printf("delete  <filename> - delete filename\n");
+		printf("send    <filename> - upload filename\n");
+	} else if (strcmp(cmd, "connect") == 0) {
+		if (sscanf(cmdl, "%*s %hx:%hx", &vid, &pid) != 2)
+			printf("connect requires a vid and pid\n");
+		else
+			command |= CMD_CONNECT;
+	} else if (strcmp(cmd, "storage") == 0) {
+		char type[4];
+		sscanf(cmdl, "%*s %4s", type);
+		if ((strcmp(type, "sd") && strcmp(type, "mem"))) {
+			printf("Requires either 'sd' or 'mem'\n");
+		} else {
+			if (strcmp(type, "sd") == 0) {
+				command &= ~DEV_INTERNAL;
+				command |= DEV_SD;
+				printf("Switching to SD Card\n");
+			} else {
+				command &= ~DEV_SD;
+				command |= DEV_INTERNAL;
+				printf("Switching to Internal Memory\n");
+			}
+		}
+	} else if(strcmp(cmd, "delete") == 0 ||
+		  strcmp(cmd, "send") == 0) {
+		free(filename);
+		filename = NULL;
+		sscanf(cmdl, "%*s %ms", &filename);
+		if (filename == NULL) {
+			printf("Requires filename\n");
+		} else {
+			if (strcmp(cmd, "delete") == 0)
+				command |= CMD_DELETE;
+			else
+				command |= CMD_SEND;
+		}
+	} else if(strcmp(cmd, "exit") == 0) {
+		ret = 1;
+	} else if (strcmp(cmd, "disconnect") == 0) {
+		command |= CMD_DISCONNECT;
+	} else if (strcmp(cmd, "model") == 0) {
+		command |= CMD_MODEL;
+	} else if (strcmp(cmd, "capacity") == 0) {
+		command |= CMD_CAPACITY;
+	} else if (strcmp(cmd, "list") == 0) {
+		command |= CMD_LIST;
+	} else {
+		ret = -1;
+	}
+	free(cmd);
+	return ret;
+}
+
+void interactive() {
+	char line[BUFSIZ];
+	int rsp, ret = 0;
+	exword_t *handle = NULL;
+
+	printf("exword interactive mode\n");
+	while (ret != 1) {
+		printf(">> ");
+		fgets(line, BUFSIZ, stdin);
+		ret = parse_commandline(line);
+
+		if (ret < 0) {
+			printf("Invalid command\n");
+			continue;
+		}
+
+		if (handle == NULL &&
+		    command & REQ_CON_MASK) {
+			printf("Not connected\n");
+			continue;
+		}
+
+		switch(command & CMD_MASK) {
+		case CMD_SCAN:
+			scan_devices();
+			break;
+		case CMD_CONNECT:
+			printf("connecting to %04X:%04X...", vid, pid);
+			handle = exword_open_by_vid_pid(vid, pid);
+			if (handle == NULL) {
+				printf("device not found\n");
+			} else {
+				if(connect(handle) != 0x20) {
+					printf("connect failed\n");
+					exword_close(handle);
+					handle = NULL;
+				} else {
+					printf("done\n");
+				}
+			}
+			break;
+		case CMD_DISCONNECT:
+			printf("disconnecting...");
+			disconnect(handle);
+			exword_close(handle);
+			handle = NULL;
+			printf("done\n");
+			break;
+		case CMD_MODEL:
+			rsp = display_model(handle);
+			if (rsp != 0x20)
+				printf("%s\n", exword_response_to_string(rsp));
+			break;
+		case CMD_LIST:
+			rsp = list_files(handle);
+			if (rsp != 0x20)
+				printf("%s\n", exword_response_to_string(rsp));
+			break;
+		case CMD_CAPACITY:
+			rsp = display_capacity(handle);
+			if (rsp != 0x20)
+				printf("%s\n", exword_response_to_string(rsp));
+			break;
+		case CMD_DELETE:
+			printf("deleting...");
+			rsp = delete_file(handle, filename);
+			printf("%s\n", exword_response_to_string(rsp));
+			break;
+		case CMD_SEND:
+			printf("uploading...");
+			rsp = send_file(handle, filename);
+			printf("%s\n", exword_response_to_string(rsp));
+			break;
+		}
+	}
+	if (handle != NULL) {
+		disconnect(handle);
+		exword_close(handle);
+	}
+	free(filename);
 }
 
 int scan_devices()
@@ -259,11 +411,16 @@ int main(int argc, const char** argv)
 	int rsp;
 	optCon = poptGetContext(NULL, argc, argv, options, 0);
 	poptGetNextOpt(optCon);
+	if (command & CMD_INTERACTIVE) {
+		interactive();
+		exit(0);
+	}
+
 	if ((command & DEV_SD) && (command & DEV_INTERNAL))
 		usage(optCon, 1, "--sd and --internal options mutually exclusive");
 	if (command == 0)
 		command |= DEV_INTERNAL;
-	if (do_scan) {
+	if (command & CMD_SCAN) {
 		scan_devices();
 		poptFreeContext(optCon);
 		exit(0);
@@ -276,7 +433,9 @@ int main(int argc, const char** argv)
 		fprintf(stderr, "Failed to open %04X:%04X\n", vid, pid);
 	} else {
 		exword_set_debug(device, debug_level);
-		switch(command & CMD_MASK) {
+		rsp = connect(device);
+		if (rsp == 0x20) {
+			switch(command & CMD_MASK) {
 			case CMD_LIST:
 				rsp = list_files(device);
 				break;
@@ -294,7 +453,9 @@ int main(int argc, const char** argv)
 				break;
 			default:
 				usage(optCon, 1, "No such command");
+			}
 		}
+		disconnect(device);
 		printf("%s\n", exword_response_to_string(rsp));
 		exword_close(device);
 	}
